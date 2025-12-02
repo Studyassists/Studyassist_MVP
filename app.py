435a67aebf5760e0c35f91f2f8a4f7a8d4f9a058
info@studyassists.com <info@studyassists.com>
2025-12-02 18:49:50 +0100

    prepare to render

--- app.py
+++ app.py
@@ -528,5 +528,11 @@ def generate_quiz():
 def results():
     return render_template("results.html")
 
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
